2a0559eeec0e8bad7cc56265cc4b9a58ab6ba00f
muhammed rifkhan <rifkhanmuhammed17@gmail.com>
2024-06-12 12:24:34 +0530

    fixed api problems

--- frontend/src/Apis/AuthRequest.js
+++ frontend/src/Apis/AuthRequest.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:5000/' })
+const API = axios.create({ baseURL: 'https://smartaccountbookv1.onrender.com/' })
 // const API = axios.create({ baseURL: 'https://account-back-4krv.onrender.com' })
 
 export const logIn = formData => API.post('/user/signin', formData)
