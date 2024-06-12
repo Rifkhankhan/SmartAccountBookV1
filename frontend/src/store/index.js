c5b5c142c55d59b56c89fb9830b8cfc5bce382e2
muhammed rifkhan <rifkhanmuhammed17@gmail.com>
2024-06-12 12:59:46 +0530

    fixed api problems-2

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,20 +1,14 @@
-import { expanseSlice } from './ExpanseSlice'
-import { receiptSlice } from './ReceiptSlice'
 import { userSlice } from './UserSlice'
 import { authSlice } from './AuthSlice'
-import { advanceSlice } from './AdvanceSlice'
-import { loanSlice } from './LoanSlice'
+
 import { accountRequestSlice } from './AccountRequestSlice'
 import { thunk } from 'redux-thunk'
 import { configureStore } from '@reduxjs/toolkit'
 import { requestSlice } from './DataActivitySlice'
 export const store = configureStore({
 	reducer: {
-		expanse: expanseSlice.reducer,
-		receipt: receiptSlice.reducer,
 		user: userSlice.reducer,
-		advance: advanceSlice.reducer,
-		loan: loanSlice.reducer,
+
 		auth: authSlice.reducer,
 		accountRequest: accountRequestSlice.reducer,
 		request: requestSlice.reducer
